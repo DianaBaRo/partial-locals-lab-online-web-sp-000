2c810e96432da78ebe1df4ee318b5940fd1a3e97
Diana Badas <diana.badas@gmail.com>
2019-06-06 12:29:18 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources: :classrooms
-  resurces: :students
+  resources :classrooms
+  resurces :students
 end
